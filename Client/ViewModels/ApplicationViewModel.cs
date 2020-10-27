36f7e21f1002a725e2283d81df97844f93fa7f9f
Andriy_Antonik <zadrotdd1995@gmail.com>
2020-10-27 15:37:59 +0200

    AddingTeachersViewModel

---
+++ Client/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,77 @@
+﻿using Client.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Client.ViewModels
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private StudentViewModel selectedStudent;
+
+        public ObservableCollection<StudentViewModel> Students { get; set; }
+        public StudentViewModel SelectedStudent
+        {
+            get { return selectedStudent; }
+            set
+            {
+                selectedStudent = value;
+                OnPropertyChanged("SelectedStudent");
+            }
+        }
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      StudentViewModel student = new StudentViewModel();
+                      Students.Insert(0, student);
+                      SelectedStudent = student;
+                  }));
+            }
+        }
+
+        // команда удаления
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      StudentViewModel student = obj as StudentViewModel;
+                      if (student != null)
+                      {
+                          Students.Remove(student);
+                      }
+                  },
+                 (obj) => Students.Count > 0));
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Students = new ObservableCollection<StudentViewModel>
+            {
+                new StudentViewModel { Id = 1, FullName = "Andriy Antonik", GroupId = 1 },
+                new StudentViewModel { Id = 2, FullName = "New Student1", GroupId = 2 },
+                new StudentViewModel { Id = 3, FullName = "New Student2", GroupId = 1 },
+
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Client/ViewModels/StudentViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Client.ViewModels
+{
+    public class StudentViewModel : INotifyPropertyChanged
+    {
+        int id;
+        string fullName;
+        int groupId;
+
+        public int Id 
+        { 
+            get => id; 
+            set {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string FullName
+        {
+            get => fullName;
+            set
+            {
+                fullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+        public int GroupId
+        {
+            get => groupId;
+            set
+            {
+                groupId = value;
+                OnPropertyChanged("GroupId");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        //  public Group Group { get; set; }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
